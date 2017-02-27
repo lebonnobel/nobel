db8cdf38e06776a1d6f571b2dbc13ed7fcf226e7
Emma Bäckström <backstromemma@gmail.com>
2017-02-27 17:19:58 +0100

    Test

--- app/js/services/worldBankService.js
+++ app/js/services/worldBankService.js
@@ -1,6 +1,12 @@
 nobelApp.factory('worldBankService', ['$window', '$http', '$q', function ($window, $http, $q) {
 	// This service takes care of the calls to the World Bank API
 
+	this.dataUrl = "http://api.worldbank.org/v2/"
+
+	$http.get(this.dataUrl + 'datacatalog' + '?format=json')
+       .then(function(res){
+          $scope.testWorldBank = res.data;                
+        });
 
 	return this;
 }]);
