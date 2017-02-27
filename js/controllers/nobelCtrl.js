ee83f680e320603134768f71cbe575536a7971d2
Emma Bäckström <backstromemma@gmail.com>
2017-02-27 19:29:14 +0100

    Added http-server

---
+++ js/controllers/nobelCtrl.js
@@ -0,0 +1,60 @@
+nobelApp.controller('nobelCtrl', 
+	function(worldBankService, nobelService, $scope, $http, $rootScope, $timeout) {
+ 	// Controller that controls the view
+ 	// Put $scope. in front of a variable name in order for the view to be able to use this variable
+	
+	//////////////////////// NOBEL DATA ////////////////////////////////
+
+	$scope.searchCountryName = "Sweden";
+	$scope.searchYear = 2016;
+	
+	// Testing, called from button onClick
+	$scope.getNobelPrizeData = function (){
+		//$scope.data = nobelService.getData();
+		//$scope.nobelData = $scope.data.$$state;		
+		$scope.nobelData = nobelService.getNobelDataForCountry($scope.searchCountryName, $scope.searchYear);
+
+		console.log($scope.nobelData);
+	}
+
+
+
+
+	//////////////////// ON LOAD ////////////////////
+	// This is a local on load function. All the variables created here will not be accessable from outside
+	//(function () {
+		// Here we can do stuff on load if we need to
+	//})();
+	
+	// This is our start function, here we can load up all initial values and tell the dataManager to start loading data
+	$scope.onStart = function(){
+		// Start loading the data
+		nobelService.loadData();
+	}
+  
+	$scope.onStart();
+
+	//////////////////////////// WORLD BANK DATA /////////////////////////////
+
+	// Shows which datasets you can choose from, specified in worldBankService
+	$scope.worldBankData = worldBankService.dataSets;
+
+	// This function is called when a wb (World bank) dataset is chosen from the dropdown list
+	$scope.onWbDataChange = function(wbDataChoice) {
+		console.log("You chose this data",wbDataChoice);
+		// only get the data if the wbDataChoice is valid
+		if (wbDataChoice !== undefined) {
+			// This function gets the data from worldbankService
+			// It uses a callback, (the 'function(d)' part), instead of waiting for the returning result
+			// the callback waits until the getData function is calling for it
+			worldBankService.getData(wbDataChoice.filename, function(d){
+				console.log("Here's your data", d);
+				$scope.wbData = d;
+			});
+				
+		} else {
+			$scope.wbData = ''
+		}
+	}
+
+});
