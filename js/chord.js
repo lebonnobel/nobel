147dcee054e106cad450a52730b05bed7fc22f40
Paeron <perjaa@kth.se>
2017-03-14 17:29:33 +0100

    Test

--- js/chord.js
+++ js/chord.js
@@ -48,7 +48,7 @@ nobelApp.controller('chord', function(nobelService, $scope) {
     d3.select("#flow").datum(chordData).call(chordDiagram);
   }
 
-  // Colours for Gender and Age.
+  // Colours for Gender and Age
 
   var genderColors = d3.scale.ordinal().range(["#969696","#656565","#8c8c8c", "#455c7c", "#f9ce66", "#a86d6d", "#826a84","#8fb588","#d89f71"]);
 
